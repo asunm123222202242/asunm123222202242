27cf220c380ab18ef3a88137d088c8e61df6fc7e
asunm123222202242 <144340403+asunm123222202242@users.noreply.github.com>
2023-09-15 17:36:34 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,19 +1,19 @@
-year = 2000
+# Python program to find the factorial of a number provided by the user.
 
-# To get year (integer input) from the user
-# year = int(input("Enter a year: "))
+# change the value for a different result
+num = 7
 
-# divided by 100 means century year (ending with 00)
-# century year divided by 400 is leap year
-if (year % 400 == 0) and (year % 100 == 0):
-    print("{0} is a leap year".format(year))
+# To take input from the user
+#num = int(input("Enter a number: "))
 
-# not divided by 100 means not a century year
-# year divided by 4 is a leap year
-elif (year % 4 ==0) and (year % 100 != 0):
-    print("{0} is a leap year".format(year))
+factorial = 1
 
-# if not divided by both 400 (century year) and 4 (not century year)
-# year is not leap year
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
 else:
-    print("{0} is not a leap year".format(year))
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
